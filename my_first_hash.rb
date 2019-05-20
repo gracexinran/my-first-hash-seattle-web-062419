85893d590c2998dae6e1442f2beb42cc5a6863e9
Xinran Che <gracexinran@gmail.com>
2019-05-20 19:15:30 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -13,7 +13,11 @@ def shipping_manifest
   the_manifest["whale bone corsets"] = 5
   the_manifest["porcelain vases"] = 2 
   the_manifest["oil paintings"] = 3
+<<<<<<< HEAD
   the_manifest
+=======
+  
+>>>>>>> a542b88ea05a594645b5edebb3d4416e0bf4acda
 
 end
 
